61ce65bf8fa47a9e605b33dc718b4260981b3ea3
wsarsou <walaahsarsour@gmail.com>
2023-06-16 09:51:13 -0400

    first push, feedback app with CSS, card, feedback components

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur distinctio impedit unde officia aperiam quaerat",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur distinctio impedit unde officia aperiam quaerat",
+  },
+
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur distinctio impedit unde officia aperiam quaerat",
+  },
+];
+
+export default FeedbackData;
