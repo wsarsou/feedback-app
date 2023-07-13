b1c7404d7ad481a6c53e7ef37f85a484c438f69e
wsarsou <walaahsarsour@gmail.com>
2023-07-13 09:53:42 -0400

    final code before deploying to netlify

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackStats({ feedback }) {
+function FeedbackStats({}) {
+  const { feedback } = useContext(FeedbackContext);
+
   // Calculate ratings avg
   let average =
     feedback.reduce((acc, cur) => {
@@ -18,7 +21,4 @@ function FeedbackStats({ feedback }) {
   );
 }
 
-FeedbackStats.propTypes = {
-  feedback: PropTypes.array.isRequired,
-};
 export default FeedbackStats;
